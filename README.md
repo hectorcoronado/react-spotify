61e4f2de8dae9e783c40920c5c1daf807c867cb6
hectorcoronado <coronado.hector@gmail.com>
2017-05-20 02:59:55 -0700

    renders & plays back song

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # react-spotify
 
 #### A practice front end app that uses Spotify's API to fetch & play songs. Based on tutorial from [this](https://www.udemy.com/learn-react-the-worlds-most-lucrative-javascript-library/learn/v4/t/lecture/6464284?start=0) Udemy course.
+
+#### Some features/libraries employed:
+- axios
+- CSS flexbox
--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import SearchBar from './SearchBar/SearchBar';
+import SongItem from './SongItem/SongItem';
 import searchSpotify from '../utils/searchSpotify';
 
 export default class App extends Component {
@@ -9,23 +10,26 @@ export default class App extends Component {
     this.state = {
       initialMessage: 'greeting',
       song: '',
+      tracks: [],
     };
   }
 
   fetchSongs = () => {
     searchSpotify(this.state.song)
-      .then((tracks) => console.log('tracks', tracks));
+      .then(({ tracks }) => this.setState({ tracks }));
   }
 
   render() {
-    const { initialMessage } = this.state;
+    const { tracks } = this.state;
+
     return (
       <div>
-        {initialMessage}
         <SearchBar
           updateText={(song) => this.setState({ song })}
           fetchSongs={this.fetchSongs}
         />
+        {/* if tracks.items exists, render component: */}
+        {tracks.items && <SongItem songData={tracks.items[0]} />}
       </div>
     );
   }
